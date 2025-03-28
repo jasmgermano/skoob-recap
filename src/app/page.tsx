3ad35dd297ad72b2d8a30bf77cb66ba76682a993
jasmine <jasmigermano@gmail.com>
2025-03-28 12:05:23 -0300

    ✨ feat: add BookStats type for tracking book extremes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+'use client';
+
+import { getUserBooks } from "@/services/skoob";
+import { useState } from "react";
+import BookStats from "@/components/bookStats";
+import { Book } from "@/types/book";
+
+type BookStats = {
+  biggest: Book | null;
+  smallest: Book | null;
+  highestRating: Book | null;
+  lowestRating: Book | null;
+};
+
 export default function Home() {  
   return (
     <div></div>
