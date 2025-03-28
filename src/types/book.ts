7203a59362a44683c1fd069c2b6700a83e732de7
jasmine <jasmigermano@gmail.com>
2025-03-28 11:58:49 -0300

    ✨ feat: add Book interface

---
+++ src/types/book.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    edicao: {
+      nome_portugues: string;
+      autor: string;
+      paginas: number;
+      capa_pequena: string;
+    };
+    ranking: number;  
+}
+  
