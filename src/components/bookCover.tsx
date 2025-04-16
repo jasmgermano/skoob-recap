2f0c4108b785d7cfb44316b298ebd6b4562cb1d1
jasmine <jasmigermano@gmail.com>
2025-04-16 13:40:51 -0300

    feat: update BookCover component to use image proxy and enhance styling

--- src/components/bookCover.tsx
+++ src/components/bookCover.tsx
@@ -1,13 +1,15 @@
 import { Book } from "@/types/book";
 
 export const BookCover = ({ book, coverHeight, coverWidth }: { book: Book, coverHeight?: number, coverWidth?: number }) => {
+    const proxiedImage = `/api/image-proxy?url=${encodeURIComponent(book.edicao.capa_grande)}`;
+    
     return (
         <img
-                src={book.edicao.capa_grande}
+                src={proxiedImage}
                 alt={book.edicao.nome_portugues}
                 width={coverWidth}
                 height={coverHeight}
-                className="custom-box-shadow h-full object-cover"
+                className="custom-box-shadow h-full object-cover rounded-sm"
          />
     );
 }
