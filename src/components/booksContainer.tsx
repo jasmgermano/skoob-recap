0fd9f09ea5e3dc2a42234f42727bc042c6841f2c
jasmine <jasmigermano@gmail.com>
2025-04-16 13:41:34 -0300

    feat: refactor BooksContainer to use forwardRef and improve type definitions

--- src/components/booksContainer.tsx
+++ src/components/booksContainer.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { forwardRef } from 'react';
 
-export const BooksContainer = ({ children, showBookStats }: { children: React.ReactNode, showBookStats: boolean }) => {
+type BooksContainerProps = React.PropsWithChildren<{
+  showBookStats: boolean;
+}>;
+
+export const BooksContainer = forwardRef<HTMLDivElement, BooksContainerProps>(
+  ({ children }, ref) => {
     return (
-        <div className={`transition-all duration-500 ease-in-out transform 
-            ${showBookStats ? "opacity-100 scale-100" : "opacity-0 scale-95"} 
-            ${!showBookStats ? "pointer-events-none h-0 overflow-hidden" : ""}
-            bg-white w-full max-w-4xl rounded-2xl flex flex-col gap-4 p-4 mt-5 sm:p-6`}
-            >
-                {children}
-            </div>
-    )
-}
+      <div
+        ref={ref}
+        className={`bg-white max-w-4xl rounded-2xl flex flex-col gap-4 p-4 mt-5 sm:p-9`}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+BooksContainer.displayName = "BooksContainer";
