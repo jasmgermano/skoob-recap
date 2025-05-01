f840fa83a28a7d4890626c6e34719733f280a98e
jasmine <jasmigermano@gmail.com>
2025-05-01 16:31:00 -0300

    feat: add Google Ads script to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const poppins = Poppins({
   variable: '--font-poppins',
@@ -20,6 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5761478072622716"
+        crossOrigin="anonymous"
+      />
       <body
         className={`${poppins.variable} antialiased`}
       >
